fdb4d593357a7e801aba2761622f40beb567e9e9
Osheit <chang.osheit@gmail.com>
2021-05-20 16:38:59 -0400

    Basic finish version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,104 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Accesories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="CUSTOM ORDER"
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="MODEL X"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="CUSTOM ORDER"
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="ORDER NOW"
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar for new Roofs"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="ORDER NOW"
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="SHOP NOW"
+          rightBtnTxt="SAMPLE"
+          rightBtnLink="LEARN MORE"
+          twoButtons="true"
+        />
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
